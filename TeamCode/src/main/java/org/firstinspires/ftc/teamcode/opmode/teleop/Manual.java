82841eaa32cda4b6de6ae51e1ebaea02933740f6
Christian Esneul <nawaplayz@gmail.com>
2025-09-20 13:59:47 -0500

    Foundation classes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/Manual.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.opmode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.RobotHardware;
+
+@TeleOp
+public class Manual extends RobotHardware {
+
+    @Override
+    public void init() {
+        super.init();
+
+    }
+
+    @Override
+    public void init_loop() {
+        super.init_loop();
+
+    }
+
+    @Override
+    public void start() {
+        super.start();
+
+    }
+
+    @Override
+    public void loop() {
+        super.loop();
+
+    }
+}
